a90f5609ae1362f2ac86f9066f0d4afe020de41b
rahulolla7255 <rahulolla7255@gmail.com>
2026-07-25 08:03:06 +0530

    speech.py

--- speech.py
+++ speech.py
@@ -1,34 +1,37 @@
 import speech_recognition as sr
 
+
 def speech_to_text():
 
     recognizer = sr.Recognizer()
 
+
     with sr.Microphone() as source:
 
-        print("Listening...")
+        stt = "Listening..."
 
-        audio = recognizer.listen(source)
+        recognizer.adjust_for_ambient_noise(
+            source,
+            duration=1
+        )
 
+        audio = recognizer.listen(
+            source,
+            timeout=10,
+            phrase_time_limit=10
+        )
 
 
     try:
 
         text = recognizer.recognize_google(
-            audio
+            audio,
+            language="en-IN"
         )
 
         return text
 
 
-    except:
+    except Exception as e:
 
-        return "Could not understand audio"
-
-
-
-if __name__ == "__main__":
-
-    print(
-        speech_to_text()
-    )
+        return None
